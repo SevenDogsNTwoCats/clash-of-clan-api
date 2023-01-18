cf579a1d88842035ff01a3b80e04cbd8d933a8d2
AE9 <78670212+SevenDogsNTwoCats@users.noreply.github.com>
2023-01-18 13:17:59 -0600

    settings edit

--- clanpage/settings.py
+++ clanpage/settings.py
@@ -81,12 +81,7 @@ WSGI_APPLICATION = 'clanpage.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite', conn_max_age=600)
-    }
-}
+    'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite', conn_max_age=600)}
 
 
 # Password validation
